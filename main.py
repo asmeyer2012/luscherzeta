d4ae753b9e89d51eea62481540e4dfdda3f9b476
Aaron Meyer <asmeyer.physics@gmail.com>
2025-08-05 14:15:54 -0700

    some cleanup, change prints to python3;

--- main.py
+++ main.py
@@ -2,82 +2,54 @@ import numpy as np
 from zeta.czeta import czeta
 from matrix_element import *
 
-#t0 = czeta()
-#
-##d = [0,1,1]
-##gam = 1.1
-#d = [0,0,0]
-#gam = 1.0
-#q2 = .9
-#
-#t0.set_dgam(d[0],d[1],d[2],gam)
-#
-#for l in range(5):
-# for m in range(-l,l+1):
-#  t0.set_lm(l,m)
-#  print l,m,t0.evaluate(q2)
 
-#lmax = 1
-##lmax = 2
-#fullString =""
-#for l0 in range(0,lmax+1):
-# for l2 in range(0,lmax+1):
-#  for l1 in range( abs(l0-l2), l0+l2+1):
-#   for m0 in range(-l0,l0+1):
-#    for m1 in range(-l1,l1+1):
-#     for m2 in range(-l2,l2+1):
-#      print 'test',l0,l1,l2,m0,m1,m2
-#      fullString = fullString +("l"+str(l0)+str(l1)+str(l2)+"m"+str(m0)+str(m1)+str(m2)+" "\
-#      +("%10.6e" % wigner3j(l0,l1,l2,m0,m1,m2))+"\n")
-#
-#print fullString
-#
-#f = open('test_py_3j','w')
-#f.write(fullString)
-#f.close()
 
-print "starting"
+## basic usage
+
 t0 = czeta()
 
-d = [0,0,0]
-gam = 1.0
-t0.set_dgam(d[0],d[1],d[2],gam)
-t0.set_lm(1,0)
+_svec = [.1,.1,0]
+_gamma = 1.1
+_q2 = .9
 
-#q2 = -1e3
-#t0.evaluate(q2)
-q2 = .9
-lmax = 2 ## maximum angular momentum to consider
-lmax2 = (lmax+1)*(lmax+1) ## maximum index of matrix
+t0.set_svec_gamma( _svec[0], _svec[1], _svec[2], _gamma)
+
+for _l in range(5):
+  for _m in range( -_l, _l+1):
+    t0.set_lm( _l, _m)
+    print( _l, _m, t0.evaluate(_q2))
+
+
 
 ## indexing for the M matrix
-def Mindex(l,m):
-  return l*(l+1)-m
-def Mitolm(i):
-  l = int( np.floor(np.sqrt(i)))
-  m = l*(l+1)-i
-  return (l,m)
 
-M = np.zeros( (lmax2, lmax2), dtype=complex)
-for i0 in range(lmax2):
- for i1 in range(lmax2):
-  l0,m0 = Mitolm(i0)
-  l1,m1 = Mitolm(i1)
-  M[i0,i1] = matrixElem(t0,l0,l1,m0,m1,q2)
+def Mindex( l, m):
+  return l*(l+1) -m
+
+def Mitolm( i):
+  l = int( np.floor( np.sqrt( i)))
+  m = l*(l+1)-i
+  return (l, m)
+
+lmax = 2 ## maximum angular momentum to consider
+lmax2 = (lmax+1)**2 ## maximum index of matrix
+M = np.zeros(( lmax2,  lmax2), dtype=complex)
+for _i0 in range( lmax2):
+  for _i1 in range( lmax2):
+    _l0, _m0 = Mitolm( _i0)
+    _l1, _m1 = Mitolm( _i1)
+    M[ _i0, _i1] = matrixElem( t0, _l0, _l1, _m0, _m1, _q2)
 
 ### for consistency checks
 #z00 = chop(M[0,0])
 #z40 = chop(M[4,8])*7./15.
-### the following must all be the same
-#print  chop(M[4, 8])*7./15.
-#print -chop(M[2,12])*7./(4.*np.sqrt(21.))
-#print  chop(M[1,11])*7./(3.*np.sqrt(14.))
-#print  chop(M[3, 9])*7./(1.*np.sqrt(210.))
+#### the following must all be the same
+#print(  chop(M[4, 8])*7./15.)
+#print( -chop(M[2,12])*7./(4.*np.sqrt(21.)))
+#print(  chop(M[1,11])*7./(3.*np.sqrt(14.)))
+#print(  chop(M[3, 9])*7./(1.*np.sqrt(210.)))
 
-print chop(M)
+print( chop(M))
 
-#for l in range(5):
-# for m in range(-l,l+1):
-#  t0.set_lm(l,m)
-#  print l,m,t0.evaluate(q2)
+
 
--- matrix_element.py
+++ matrix_element.py
@@ -3,39 +3,7 @@ import numpy as np
 from scipy.special import gamma
 from sympy.physics.wigner import gaunt
 
-### triangle coefficient
-#def triCoef(l0,l1,l2):
-#  return gamma(l0+l1-l2+1) *gamma(l0-l1+l2+1) *gamma(-l0+l1+l2+1) /gamma(l0+l1+l2+2)
-#
-### \sum_t (-1)^t / ( t! (l2-l1+t+m0)! (l2-l0+t-m1)! (l0+l1-l2-t)! (l0-t-m0)! (l1-t-m1)!)
-### t is summed only over terms where all arguments of factorials is positive
-### max t = nu can be computed before evaluation
-#def xSum(l0,l1,l2,m0,m1,m2):
-#  nu = min( l0+m0, l0-m0, l1+m1, l1-m1, l2+m2, l2-m2, l0+l1-l2, l0-l1+l2, -l0+l1+l2)
-#  xsum = 0.
-#  for t in range(nu+1): ## nu+1 terms?
-#    print nu,": ",t,(l2-l1+t+m0),(l2-l0+t-m1),(l0+l1-l2-t),(l0-t-m0),(l1-t-m1)
-#    xsum += np.power(-1.,t)/( \
-#      gamma(t+1) *gamma(l2-l1+t+m0+1) *gamma(l2-l0+t-m1+1) \
-#      *gamma(l0+l1-l2-t+1) *gamma(l0-t-m0+1) *gamma(l1-t-m1+1) )
-#  return xsum
-#
-### compute 3j from Racah formula
-### implementation is not working
-#def wigner3j(l0,l1,l2,m0,m1,m2):
-#  return np.power(-1.,l0-l1-m2) \
-#    *np.sqrt( triCoef(l0,l1,l2) *gamma(l0+m0+1) *gamma(l0-m0+1) \
-#    *gamma(l1+m1+1) *gamma(l1-m1+1) *gamma(l2+m2+1) *gamma(l2-m2+1) ) \
-#    *xSum(l0,l1,l2,m0,m1,m2)
 
-## clebsch-gordan coefficient calculation
-## gaunt is \propto WigJ * WigJ
-def tensorC(l0,l1,l2,m0,m1,m2):
-  #iphase = cmath.exp( complex(0., .5*np.pi *(l0-l1+l2)) )
-  #return np.power(-1.,m2) *np.sqrt( (2.*l0+1) *(2.*l1+1) *(2.*l2+1) ) \
-  #  *wigner3j(l0,l1,l2,0,0,0) *wigner3j(l0,l1,l2,m0,m1,m2) *iphase
-  iphase = np.sqrt(4.*np.pi)*np.power(-1.,m2)* cmath.exp( complex(0., .5*np.pi *(l0-l1+l2)) )
-  return gaunt(l0,l1,l2,m0,m1,m2) *iphase
 
 ## if abs2 is less than some tolerance, return 0. of the same type
 def isSmall(x):
@@ -55,6 +23,12 @@ def chop(x):
     if isSmall(x):
       return x*0.
   return x
+
+## clebsch-gordan coefficient calculation
+## gaunt is \propto WigJ * WigJ
+def tensorC(l0,l1,l2,m0,m1,m2):
+  iphase = np.sqrt(4.*np.pi)*np.power(-1.,m2)* cmath.exp( complex(0., .5*np.pi *(l0-l1+l2)) )
+  return gaunt(l0,l1,l2,m0,m1,m2) *iphase
 
 ## computation of matrix elements
 ## cz is a czeta class implementation
